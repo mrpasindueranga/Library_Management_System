1ac8c2c8f5c61a9d1e3e11b8ee2012bdb87c6bb3
mrpasindueranga <mr.h.d.pasindueranga@gmail.com>
2021-01-10 21:21:17 +0530

    Implement search query for category component

--- component/profile/category.php
+++ component/profile/category.php
@@ -17,18 +17,18 @@
   <?php require_once '../../views/profile/navbar.php'?>
   <?php if(!isset($_GET['v_id']) && !isset($_GET['cr_id']) && !isset($_GET['d_id'])):?>
   <div class="find">
-    <div class="search">
-      <input id="search" type="text" placeholder="Find a category...">
-      <a href="">
-        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-search"><circle cx="11" cy="11" r="8"></circle><line x1="21" y1="21" x2="16.65" y2="16.65"></line></svg>
-      </a>
-    </div>
+    <form action="./category.php">
+      <div class="search">
+        <input id="search" name="q" type="text" placeholder="Find a category...">
+        <button type="submit">
+          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-search"><circle cx="11" cy="11" r="8"></circle><line x1="21" y1="21" x2="16.65" y2="16.65"></line></svg>
+        </button>
+      </div>
+    </form>
     <a href="./category.php?cr_id=true">New</a>
   </div>
   <div class="card_container">
-    <?php
-        require_once '../../db/category_card.php';
-     ?>
+    <?php require_once '../../db/category_card.php';?>
   </div>
   <?php endif;?>
   <?php if(isset($_GET['v_id']) || isset($_GET['cr_id'])){
