8ebbef6263da6392bb85a67a7cba6e4fd5f76545
mrpasindueranga <mr.h.d.pasindueranga@gmail.com>
2021-01-19 10:10:39 +0530

    Restricted librarian show in member list

--- db/Conn.php
+++ db/Conn.php
@@ -1,11 +1,10 @@
 <?php
-    $host = 'localhost';
-    $user = 'root';
-    $pass = '';
-    $dbname = 'lowa';
-    
-    $conn = new mysqli($host, $user, $pass, $dbname);
-    if ($conn->connect_error) {
-      die("Connection failed: " . $conn->connect_error);
-    }
-?>
+$host = 'localhost';
+$user = 'root';
+$pass = '';
+$dbname = 'lowa';
+
+$conn = new mysqli($host, $user, $pass, $dbname);
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
