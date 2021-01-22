4576e090c283550fe785d35d6af313941ec93299
mrpasindueranga <mr.h.d.pasindueranga@gmail.com>
2021-01-22 15:18:11 +0530

    .gitignore updated

--- public/dashboard.php
+++ public/dashboard.php
@@ -1,38 +1,41 @@
 <?php
-  session_start();
-  if ($_SESSION):
+session_start();
+if ($_SESSION) :
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="./resources/css/prop.css">
-  <link rel="stylesheet" href="./resources/css/navbar.css">
-  <link rel="stylesheet" href="./resources/css/dashboard.css">
-  <script src="../views/js/navbar.js" defer></script>
-  <title>Lowa State University | E-Library</title>
-</head>
-<body>
-  <?php require_once '../views/navbar.php';?>
-  <section class="profile">
-    <div class="info">
-      <?php require_once '../views/profile/info.php';?>
-    </div>
-    <div class="content">
-      <iframe src="../component/profile/dashboard.php" frameborder="0"></iframe>
-    </div>
-  </section>
-</body>
-</html>
+  <!DOCTYPE html>
+  <html lang="en">
+
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./resources/css/prop.css">
+    <link rel="stylesheet" href="./resources/css/navbar.css">
+    <link rel="stylesheet" href="./resources/css/dashboard.css">
+    <script src="../views/js/navbar.js" defer></script>
+    <title>Lowa State University | E-Library</title>
+  </head>
+
+  <body>
+    <?php require_once '../views/navbar.php'; ?>
+    <section class="profile">
+      <div class="info">
+        <?php require_once '../views/profile/info.php'; ?>
+      </div>
+      <div class="content">
+        <iframe src="../component/profile/dashboard.php" frameborder="0"></iframe>
+      </div>
+    </section>
+  </body>
+
+  </html>
 
 <?php
-  endif;
+endif;
 ?>
 
 <?php
-  if (!$_SESSION){
-    header('location: ../public/index.php');
-  }
+if (!$_SESSION) {
+  header('location: ../public/index.php');
+}
 ?>
