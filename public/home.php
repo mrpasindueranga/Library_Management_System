ea20fca29d9d751f2eaeb6f61f5d387aa3e50dd9
mrpasindueranga <mr.h.d.pasindueranga@gmail.com>
2021-02-02 12:55:27 +0530

    Book CRUD Implement

--- public/home.php
+++ public/home.php
@@ -1,33 +1,36 @@
 <?php
-  session_start();
-  if ($_SESSION):
+session_start();
+if ($_SESSION) :
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="./resources/css/prop.css">
-  <link rel="stylesheet" href="./resources/css/navbar.css">
-  <script src="../views/js/navbar.js" defer></script>
-  <title>Lowa State University | E-Library</title>
-</head>
-<body>
-  <?php
+  <!DOCTYPE html>
+  <html lang="en">
+
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./resources/css/prop.css">
+    <link rel="stylesheet" href="./resources/css/navbar.css">
+    <script src="../views/js/navbar.js" defer></script>
+    <title>Lowa State University | E-Library</title>
+  </head>
+
+  <body>
+    <?php
     require_once '../views/navbar.php';
-  ?>
-  <?php
+    ?>
+    <?php
     require_once '../component/explore/newBooks.php';
-  ?>
-</body>
-</html>
+    ?>
+  </body>
+
+  </html>
 <?php
-  endif;
+endif;
 ?>
 
 <?php
-  if (!$_SESSION){
-    header('location: ../public/index.php');
-  }
+if (!$_SESSION) {
+  header('location: ../public/index.php');
+}
 ?>
--- public/resources/js/landing.js
+++ public/resources/js/landing.js
@@ -9,7 +9,7 @@ const signupBtn = document.querySelector('.signup-btn');
 const signinBtn = document.querySelector('.signin-btn');
 
 
-signinBtn.addEventListener('click',(event)=>{
+signinBtn.addEventListener('click', (event) => {
   const signin = document.querySelector(".signin-wrap");
   const signup = document.querySelector(".signup-wrap");
   signin.id = 'login-remove';
@@ -23,7 +23,7 @@ signinBtn.addEventListener('click',(event)=>{
   event.preventDefault();
 })
 
-signupBtn.addEventListener('click', (event) =>{
+signupBtn.addEventListener('click', (event) => {
   const signin = document.querySelector(".signin-wrap");
   const signup = document.querySelector(".signup-wrap");
   signin.id = 'login-remove';
@@ -37,21 +37,20 @@ signupBtn.addEventListener('click', (event) =>{
   event.preventDefault();
 })
 
-formLogin.addEventListener('click',(event)=>{
+formLogin.addEventListener('click', (event) => {
   // event.preventDefault();
 })
 
-regBtn.addEventListener('click',(event)=>{
+regBtn.addEventListener('click', (event) => {
   var data = document.querySelector('#captureData').attributes('value');
   event.preventDefault();
 })
 
-//TODO : home navigation
-home.addEventListener('click',(event) => {
+home.addEventListener('click', (event) => {
   const signin = document.querySelector(".signin-wrap");
   const signup = document.querySelector(".signup-wrap");
   const slideLeft = document.querySelector(".slide-left");
-  if(slideLeft){
+  if (slideLeft) {
     landing.id = 'slide-right';
     signin.id = 'login-remove';
     signup.id = 'signin-remove';
@@ -62,46 +61,46 @@ home.addEventListener('click',(event) => {
   event.preventDefault();
 })
 
-login.addEventListener('click',(event) => {
- const signin = document.querySelector(".signin-wrap");
- const signup = document.querySelector(".signup-wrap");
- const slideLeft = document.querySelector(".slide-left");
- removeSignup();
- appendSignin();
- if(!slideLeft){
-  landing.removeAttribute('id');
-  signin.removeAttribute('id');
-  signup.removeAttribute('id');
-  landing.classList.add('slide-left');
-  signin.classList.add('login-appear');
-  signup.classList.add('signin-appear');
-}
+login.addEventListener('click', (event) => {
+  const signin = document.querySelector(".signin-wrap");
+  const signup = document.querySelector(".signup-wrap");
+  const slideLeft = document.querySelector(".slide-left");
+  removeSignup();
+  appendSignin();
+  if (!slideLeft) {
+    landing.removeAttribute('id');
+    signin.removeAttribute('id');
+    signup.removeAttribute('id');
+    landing.classList.add('slide-left');
+    signin.classList.add('login-appear');
+    signup.classList.add('signin-appear');
+  }
 })
 
 function appendSignin() {
   const signin = document.querySelector('.signin-wrap');
-  if (signin.classList.contains('display-none')){
+  if (signin.classList.contains('display-none')) {
     signin.classList.remove('display-none')
   }
 }
 
 function appendSignup() {
   const signup = document.querySelector('.signup-wrap');
-  if (signup.classList.contains('display-none')){
+  if (signup.classList.contains('display-none')) {
     signup.classList.remove('display-none')
   }
 }
 
 function removeSignup() {
   const signup = document.querySelector('.signup-wrap');
-  if(!signup.classList.contains('display-none')){
+  if (!signup.classList.contains('display-none')) {
     signup.classList.add('display-none');
   }
 }
 
 function removeSignin() {
   const signin = document.querySelector('.signin-wrap');
-  if(!signin.classList.contains('display-none')){
+  if (!signin.classList.contains('display-none')) {
     signin.classList.add('display-none');
   }
 }
